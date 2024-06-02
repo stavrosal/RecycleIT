dfea3a5fbdd4f092158df789e869c9c4f208bd3d
Stavros Almpanopoulos <102695313+stavrosal@users.noreply.github.com>
2024-06-02 18:05:00 +0300

    Upload logUser.php
    PHP file for log-in

---
+++ db_files/logUser.php
@@ -0,0 +1,24 @@
+<?php
+	 $data = array();
+	 $user = $_GET["username"];
+	 $password = $_GET["password"];
+	 $host="localhost";
+	 $uname="root";
+	 $pass="";
+	 $dbname="users";
+	 $dbh = mysqli_connect($host,$uname,$pass) or die("cannot connect");
+	 mysqli_select_db($dbh, $dbname);
+	 
+	 $sql = "SELECT username, password, isAdmin FROM users WHERE username =  '" . $user . "' AND password =  '" . $password . "'";
+
+	 $result = mysqli_query($dbh, $sql);
+	 
+	 while ($row = mysqli_fetch_array($result)) {
+		$data[$row['username']] = $row['password'].$row['isAdmin'];
+	}
+	header("Content-Type: application/json");
+	echo json_encode($data);
+
+	 mysqli_close($dbh);
+?>
+
