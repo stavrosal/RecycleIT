de7a0e4ef7fe0d47ffb636feb994a5c0acc4daec
Dimvarnas <127050463+Dimvarnas@users.noreply.github.com>
2024-06-07 21:16:32 +0300

    Update regUser.php

--- db_files/regUser.php
+++ db_files/regUser.php
@@ -4,16 +4,27 @@
 	 $password = $_GET["password"];
 	 $isAdmin = $_GET["isAdmin"];
 	 $points = $_GET["points"];
+	 $paper = $_GET["paper"];
+	 $glass = $_GET["glass"];
+	 $metal = $_GET["metal"];
+	 $plastic = $_GET["plastic"];
+	 $current_paper = $_GET["current_paper"];
+	 $current_glass = $_GET["current_glass"];
+	 $current_metal = $_GET["current_metal"];
+	 $current_plastic = $_GET["current_plastic"];
 	 $host="localhost";
 	 $uname="root";
 	 $pass="";
 	 $dbname="users";
 	 $dbh = mysqli_connect($host,$uname,$pass) or die("cannot connect");
 	 mysqli_select_db($dbh, $dbname);
+	 //SELECT COUNT(username) AS count FROM users WHERE username = '' AND password = ''
 	 
 	 $exists = "SELECT COUNT(username) AS count FROM " . $dbname . " WHERE username = '" . $user . "'";
-	 $sql = "INSERT INTO " . $dbname . " VALUES ('" . $user . "' , '" . $password . "' , '" . $isAdmin . "' , '" .  $points . " ')";
+	 $sql = "INSERT INTO " . $dbname . " VALUES ('" . $user . "' , '" . $password . "' , '" . $isAdmin . "' , '" .  $points . " ' , '" .  $paper . " ' , '" .  $glass . " ' , '" .  $metal . " ' , '" .  $plastic . " ' ,  '" .  $current_paper . " ' , '" .  $current_glass . " ' , '" .  $current_metal . " ' , '" .  $current_plastic . " ')";
 	 
+	 //echo $exists;
+	 //echo $sql;
 	 
 	 //Check if account, already exists and return -1 if it does
 	 $doesExist = mysqli_query($dbh, $exists);
